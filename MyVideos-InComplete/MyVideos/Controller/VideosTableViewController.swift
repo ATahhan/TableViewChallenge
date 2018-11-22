c77465977e06d45a9b1162fe9937890b98178435
Waad Alkatheri <walquhum@stu.kau.edu.sa>
2018-11-22 05:47:31 +0300

    Add files via upload

---
+++ MyVideos-InComplete/MyVideos/Controller/VideosTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  VideosTableViewController.swift
+//  MyVideos
+//
+
+
+import UIKit
+
+class VideosTableViewController: UITableViewController
+{
+    
+    //TODO: Call fetchVideos() Function
+
+    
+    
+    static func fetchVideos() -> [Video] {
+        let v1 = Video(authorName: "David Tran", videoFileName: "v1", description: "This is an example of local video.", thumbnailFileName: "v1")
+        let v2 = Video(authorName: "Tracy Noah", videoFileName: "v2", description: "This is an example of local video.", thumbnailFileName: "v2")
+        let v3 = Video(authorName: "Brendon Burchard", videoFileName: "v3", description: "This is an example of local video.", thumbnailFileName: "v3")
+        let v4 = Video(authorName: "Elon Musk", videoFileName: "v4", description: "This is an example of local video.", thumbnailFileName: "v4")
+        let v5 = Video(authorName: "David Tran", videoFileName: "v5", description: "This is an example of local video.", thumbnailFileName: "v5")
+        let v6 = Video(authorName: "Steve Jobs", videoFileName: "v6", description: "This is an example of local video.", thumbnailFileName: "v6")
+        
+        return [v1, v2, v3, v4, v5, v6]
+    }
+    
+    
+       //TODO : Implement TableView Method
+       // TODO: return number of videos in array of Video
+
+    
+        // TODO : Connect collection view with cell
+        // TODO : connect cell with name of Video and Video Image  and return Cell
+    
+    
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
---
+++ MyVideos-InComplete/MyVideos/Model/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  Play Local Videos
+//
+
+import Foundation
+
+struct Video
+{
+    let authorName: String
+    let videoFileName: String
+    let description: String
+    let thumbnailFileName: String
+
+    
+}
+
